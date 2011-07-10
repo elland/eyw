bbca4cb853d6f18516a93c59e905fe89e38fa9c4
Igor Ranieri Elland <igor.ranieri@gmail.com>
2011-07-10 09:30:20 -0300

    first

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ActiveRecord::Base
+	belongs_to :author, :class_name => "User"
+	belongs_to :category
+	has_and_belongs_to_many :tags
+	has_many :comments
+	
+	has_friendly_id :title, :use_slug => true
 end
